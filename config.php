67f6d7c595107410b0e5fd7678dbea5d85e8fb58
Hoai Vu <hoaivu1224@gmail.com>
2019-10-06 10:40:24 +0700

    Initial commit

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+ 
+define('DBUSER','dt_admin');
+define('DBPWD','DayLaMatKhau');
+define('DBHOST','localhost');
+define('DBNAME','DT');
+$db = mysqli_connect(DBHOST,DBUSER,DBPWD,DBNAME);
+  
+?>
